2a147e3e0de4de0e8752bf4d0e10bbc723a81c3d
ZikXewen <datasup12@gmail.com>
2022-06-21 19:04:56 +0700

    fix akinator

--- src/commands/akinator.ts
+++ src/commands/akinator.ts
@@ -1,14 +1,12 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import aki from 'discord.js-akinator'
 import { Command } from '../types.js'
-import { toEmbed } from '../utils.js'
 
 const akinator: Command = {
   data: new SlashCommandBuilder()
     .setName('akinator')
     .setDescription('Start an Akinator game.'),
   exec: async (interaction) => {
-    interaction.reply({ ...toEmbed('Starting game...'), ephemeral: true })
     aki(interaction, { useButtons: true, embedColor: 'GREY' })
   },
 }
